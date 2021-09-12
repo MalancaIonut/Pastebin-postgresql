bdc109feb62a5269c52114267e48f44dac758d94
Ionut Malanca <malancaionut.mi@gmail.com>
2021-09-12 10:26:07 +0200

    add querys

--- api/querys.js
+++ api/querys.js
@@ -1,38 +1,42 @@
 const pool = require("./db_connection");
 
-const getPaste = (request, response) => {
-    pool.query('SELECT title FROM pastes ORDER BY id ASC', (error, results) => {
+const getPastes = () => {
+    return new Promise(function(resolve, reject) {
+      pool.query('SELECT * FROM pastes ORDER BY id ASC', (error, results) => {
         if (error) {
-            throw error;
+          reject(error);
         }
-        response.status(200).json(results.rows);
-    })
-}
+        resolve(results.rows);
+      });
+    });
+  }
 
-const createPaste = (request, response) => {
-    const {title, content} = request.body;
-    
-    pool.query('INSERT INTO pastes (title, content) VALUES ($1, $2)', [title, content], (error, results) => {
+const createPaste = (body) => {
+    return new Promise(function(resolve, reject) {
+      const { title, content } = body;
+      pool.query('INSERT INTO pastes (title, content) VALUES ($1, $2) RETURNING *', [title, content], (error, results) => {
         if (error) {
-            throw error;
+          reject(error);
         }
-        response.status(201).send(`User added with ID : ${result.insertId}`);
-    })
-}
-
-const deletePaste = (request, response) => {
-    const id = parseInt(request.params.id);
-
-    pool.query('DELETE FROM pastes WHERE id = $1', [id], (error, results) => {
+        resolve(`A new paste has been added with title: `);
+      });
+    });
+  }
+  
+  const deletePaste = (pasteId) => {
+    return new Promise(function(resolve, reject) {
+      const id = parseInt(pasteId);
+      pool.query('DELETE FROM pastes WHERE id = $1', [id], (error, results) => {
         if (error) {
-            throw error;
+          reject(error);
         }
-        response.status(200).send(`User deleted with ID : ${id}`);
-    })
-}
+        resolve(`Paste deleted with ID: ${id}`);
+      });
+    });
+  }
 
 module.exports = {
-    getPaste,
+    getPastes,
     createPaste,
     deletePaste,
 };
