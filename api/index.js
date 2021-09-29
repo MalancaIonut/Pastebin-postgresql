43bb8bb43a418e565586fb3fca806584f858f97c
Malanca Ionut <83650057+MalancaIonut@users.noreply.github.com>
2021-09-29 12:34:19 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const db = require("./querys");
+const db = require("./queries");
 
 app.use(express.json());
 app.use (
